986d961dde18fb16d78e70bd5473f7095058997d
Aanchalll <98saanchal@gmail.com>
2024-01-07 16:27:08 +0530

    category card issue

--- pages/product-category/[category].tsx
+++ pages/product-category/[category].tsx
@@ -9,7 +9,7 @@ import { itemType } from "../../context/cart/cart-types";
 import DownArrow from "../../public/icons/DownArrow";
 import NoDataFound from "../no-data";
 import { product_categories } from "../../messages/common/constants";
-import CategoryCard from "./category-card";
+// import CategoryCard from "./category-card";
 
 type OrderType = "latest" | "price" | "price-desc";
 
@@ -302,3 +302,103 @@ const Products = [
     img2: "/bg-img/CNC-Cutting.webp",
   },
 ];
+
+// import { useTranslations } from "next-intl";
+import Image from "next/image";
+import styles from "../../components/Card/Card.module.css";
+// import {itemType } from "../../context/cart/cart-types";
+import { FC } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation, Autoplay]);
+
+type Props2 = {
+  item: itemType | any;
+};
+
+const CategoryCard: FC<Props2> = ({ item }) => {
+  const t = useTranslations("CartWishlist");
+  const { name, products, category } = item;
+  const sliderArray =
+    products && products.length > 3 ? products?.slice(0, 3) : products;
+  return (
+    <div className={styles.card}>
+      <div className={styles.imageContainer}>
+        <div className="text-silver text-4xl text-center relative top-3 z-30">
+          <a
+            href={`/products/${category}`}
+            // extraClass="absolute bottom-10-per sm:right-10-per z-20"
+          >
+            {name}
+          </a>
+        </div>
+        <Slideshow name={name} products={sliderArray} />
+      </div>
+    </div>
+  );
+};
+// export default CategoryCard;
+
+const Slideshow = ({ name = "", products = [] }) => {
+  // const t = useTranslations("Index");
+
+  return (
+    <>
+      <div
+        className="relative slide-container w-full h-full z-20  
+      overflow-hidden border-2 border-gray800 rounded-3xl "
+      >
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={0}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          navigation={false}
+          pagination={false}
+          className="mySwiper"
+        >
+          {products?.map((slider: any) => (
+            <SwiperSlide key={slider.id}>
+              <div
+                className=" 
+              lg:block 
+              "
+              >
+                <Image
+                  layout="responsive"
+                  src={slider.src}
+                  width={"100%"}
+                  height={"100%"}
+                  alt={name}
+                />
+              </div>
+              {/* <div className="hidden sm:block lg:hidden">
+                <Image
+                  layout="responsive"
+                  src={slider.src}
+                  width={820}
+                  height={720}
+                  alt={name}
+                />
+              </div>
+              <div className="sm:hidden">
+                <Image
+                  layout="responsive"
+                  src={slider.src}
+                  width={428}
+                  height={800}
+                  alt={name}
+                />
+              </div> */}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  );
+};
